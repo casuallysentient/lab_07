31c1bf8900269b461fbfdb9f81867c7dd41a1a1d
Reilly <reilly.ford@gmail.com>
2021-03-27 11:24:09 -0400

    exercise 4

--- haiku.py
+++ haiku.py
@@ -5,7 +5,17 @@ def is_haiku(input_string):
     :param input_string: A string
     :return: True or False
     """
-    pass
+    split_lines = input_string.split("/")
+    if len(split_lines) == 3:
+        line_1 = split_lines[0]
+        words_1 = line_1.split(',')
+        line_2 = split_lines[1]
+        words_2 = line_2.split(',')
+        line_3 = split_lines[2]
+        words_3 = line_3.split(',')
+        if len(words_1) == 5 and len(words_2) == 7 and len(words_3) == 5:
+            return True
+    return False
 
 
 def haiku_string_parser(input_string):
@@ -16,7 +26,18 @@ def haiku_string_parser(input_string):
     :param input_string: A single-line string representing a haiku's syllabic and line division.
     :return: A multi-line representation of input_string.
     """
-    pass
+    split_lines = input_string.split("/")
+    if len(split_lines) == 3:
+        line_1 = split_lines[0]
+        words_1 = line_1.split(',')
+        line_2 = split_lines[1]
+        words_2 = line_2.split(',')
+        line_3 = split_lines[2]
+        words_3 = line_3.split(',')
+        if len(words_1) == 5 and len(words_2) == 7 and len(words_3) == 5:
+            return ("".join(words_1)).rstrip() + "\n" + ("".join(words_2)).rstrip() + "\n" + ("".join(words_3)).rstrip()
+    return ""
+
 
 def main():
     """
@@ -28,8 +49,8 @@ def main():
     elif is_haiku(haiku_string) is not None and not is_haiku(haiku_string):
         print("This is not a valid haiku based on its structure.")
 
-    # formatted_haiku = haiku_string_parser(haiku_string)  # Optional
-    # print(formatted_haiku)                               # Optional
+    formatted_haiku = haiku_string_parser(haiku_string)  # Optional
+    print(formatted_haiku)                               # Optional
 
 
 # DO NOT WRITE CODE BELOW THIS LINE
